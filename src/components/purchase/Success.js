ed4037b510bbee76e3d001db046439a5eafe06bf
llabuzie <60725482+llabuzie@users.noreply.github.com>
2020-11-04 14:53:25 -0500

    removed dialogue

--- src/components/purchase/Success.js
+++ src/components/purchase/Success.js
@@ -19,15 +19,11 @@ export class Success extends Component {
     return (
       <MuiThemeProvider>
         <>
-          <Dialog
-            open
-            fullWidth
-            maxWidth='sm'
-          >
+          
             <AppBar title="Success" />
             <h1>Thank You For Your Submission</h1>
             <p>You will get an email with further instructions.</p>
-          </Dialog>
+
         </>
       </MuiThemeProvider>
     );
