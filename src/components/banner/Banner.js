6a40ce581888c0c56520f3f231f51266bfd7aa68
Niranjan Abhyankar <niranjanabhyankar88@gmail.com>
2020-11-03 23:17:16 -0500

    Testing maps

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -38,10 +38,7 @@ function Banner() {
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
 
-        <div className="banner_buttons">
-          <button className="banner_button">Play</button>
-          <button className="banner_button">My List</button>
-        </div>
+        
         <h1 className="banner_description">{truncate(movie?.overview, 200)}</h1>
         
       </div>   
